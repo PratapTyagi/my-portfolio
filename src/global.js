ee5a24dd50beb98beb22e6581c23b06051a46dab
PratapTyagi <tyagipratap111@gmail.com>
2022-07-17 22:19:43 +0530

    home page basic implementation

--- src/global.js
+++ src/global.js
@@ -5,11 +5,13 @@ export const GlobalStyles = createGlobalStyle`
   *::after,
   *::before {
     box-sizing: border-box;
+    padding: 0;
+    margin: 0;
   }
 
   body {
     align-items: center;
-    background: ${({ theme }) => theme.body};
+    background: whitesmoke;
     color: ${({ theme }) => theme.text};
     display: flex;
     font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,33 @@
 import Programmer from "../../assets/programmer.svg";
 import styles from "./Home.module.css";
 
+import Type from "./Type";
+
 const Home = () => {
   return (
     <div className={styles.home}>
+      <div className={styles.left}>
+        <h1>Pratap Tyagi</h1>
+        <div
+          style={{
+            fontSize: "25px",
+            paddingTop: 30,
+            paddingLeft: 5,
+            color: "grey",
+          }}
+        >
+          <Type />
+        </div>
+        <a
+          className={styles.watch_resume}
+          href="https://drive.google.com/file/d/14XOQmNxE0nM_EXiJhySD_x8JE9UyoU1Q/view?usp=sharing"
+          target="_blank"
+        >
+          <hr />
+          <span class="material-symbols-outlined">play_circle</span>
+          <p>Watch resume</p>
+        </a>
+      </div>
       <img
         src={Programmer}
         alt="programmer"
@@ -11,14 +35,6 @@ const Home = () => {
         width={650}
         title="programmer"
       />
-      <div className={styles.right}>
-        <p>
-          <span className="theme">Hi</span> There!
-        </p>
-        <p>
-          I'm <span className="theme">Pratap Tyagi</span>
-        </p>
-      </div>
     </div>
   );
 };
