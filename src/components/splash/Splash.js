a0d03559046a1eff277e047a9412395e56bd94a8
PratapTyagi <tyagipratap111@gmail.com>
2022-07-27 10:06:36 +0530

    redirect logic

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+
 import "./Splash.css";
 import SplashGif from "../../assets/splash.gif";
 
+import { Navigate } from "react-router-dom";
+
 const Splash = () => {
-  return (
+  const [isRedirect, setIsRedirect] = useState(false);
+
+  useEffect(() => {
+    let id = setTimeout(() => setIsRedirect(true), 5500);
+    return () => {
+      clearTimeout(id);
+    };
+  }, []);
+
+  return isRedirect ? (
+    <Navigate to="/home" />
+  ) : (
     <div className="splash">
       <img src={SplashGif} alt="Splash" />
     </div>
